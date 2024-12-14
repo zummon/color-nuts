d10246091453257a7aa6938ab5580865864a8d5b
zummon <zummon.space@gmail.com>
2024-12-14 21:29:36 +0700

    save

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,54 @@
 <script>
   import { onMount } from "svelte";
+
   let chooseknots = $state([]);
-  let screws = $state([
-    ["#ef4444", "#ef4444", "#ef4444", "#ef4444"],
-    ["#f59e0b", "#f59e0b", "#f59e0b", "#f59e0b"],
-    ["#22c55e", "#22c55e", "#22c55e", "#22c55e"],
-    ["#14b8a6", "#14b8a6", "#14b8a6", "#14b8a6"],
-    ["#3b82f6", "#3b82f6", "#3b82f6", "#3b82f6"],
-    ["#8b5cf6", "#8b5cf6", "#8b5cf6", "#8b5cf6"],
-    ["", "", "", ""],
-    ["", "", "", ""],
-  ]);
-  function shuffle() {
-    let array = screws.slice(0, -2).flat();
+  let screws = $state([]);
+
+  function shuffle({ colors, sizes, extras }) {
+    let array = []
+    let result = []
+    sizes.forEach((size, index) => {
+      array.push(... new Array(size).fill(colors[index]))
+    })
 
     // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
     let currentIndex = array.length;
-
     // While there remain elements to shuffle...
     while (currentIndex != 0) {
       // Pick a remaining element...
       let randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-
       // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex],
-        array[currentIndex],
-      ];
-    }
-    //
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    } //
 
-    screws.slice(0, -2).forEach((knots, index) => {
-      knots.forEach((knot, idx) => {
-        screws[index][idx] = array.pop();
-      });
+    sizes.forEach((size, index) => {
+      result[index] = { knots: [], size: size };
+      for (let _ of new Array(size)) {
+        result[index].knots.push(array.pop())
+      }
     });
+    extras.forEach((extra) => {
+      result.push({ knots: [], size: extra })
+    })
+    
+    screws = result 
   }
   onMount(() => {
-    shuffle();
+    let params = new URLSearchParams(location.search)
+    let colors = params.getAll('color')
+    let sizes = params.getAll('size')
+    let extras = params.getAll('extra')
+    if (!colors[0]){
+      colors = ['#ef4444','#f59e0b','#84cc16','#14b8a6','#3b82f6','#8b5cf6']
+    } 
+    if (!sizes[0]) {
+      sizes = [4,4,4,4,4,4]
+    }
+    if (!extras[0]) {
+      extras = [4,4]
+    }
+    shuffle({ colors, sizes, extras });
   });
 </script>
 
@@ -56,59 +66,23 @@
 </div>
 
 <div class="flex flex-wrap gap-4 px-4">
-  {#each screws as knots, index (index)}
+  {#each screws as { knots, size }, index (index)}
     <button
       class="flex flex-col gap-2"
       onclick={() => {
-        let ready = [];
-        let position;
         if (chooseknots[0]) {
-          let count = 0;
-          for (let knot of knots.slice()) {
-            if (knot) {
-              break;
-            } else {
-              count += 1;
-            }
-          }
-          if (chooseknots.length <= count) {
-            screws[index].splice(
-              screws[index].length - chooseknots.length,
-              chooseknots.length,
-              ...chooseknots.slice()
-            );
-            chooseknots = [];
-          }
+          let part = chooseknots.pop()
+          screws[index].knots.push(part)
         } else {
-          let idx = 0;
-          for (let knot of knots.slice()) {
-            if (knot) {
-              if (position === undefined) {
-                position = idx;
-              }
-              if (!ready[0] || ready[0] == knot) {
-                ready.push(knot);
-              } else {
-                break;
-              }
-            } else {
-              break;
-            }
-            idx += 1;
-          }
-          chooseknots = ready;
-          screws[index].splice(
-            position,
-            ready.length,
-            ...ready.slice().fill("")
-          );
+          let part = screws[index].knots.pop()
+          chooseknots.push(part)
         }
       }}
     >
-      {#each knots as knot, idx (`${index}-${idx}`)}
+      {#each new Array(size) as _, idx (`${index}-${idx}`)}
         <span
           class="h-8 w-16 rounded-full"
-          style="background-color: {knot || '#9ca3af'};"
+          style="background-color: {knots[idx] || '#9ca3af'};"
         ></span>
       {/each}
     </button>
