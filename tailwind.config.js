b6d76b7074e333be082dd40bdcd1363cdf28fe25
zummon <zummon.space@gmail.com>
2025-03-15 09:55:44 +0700

    fix css cdn not load

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-export default {}
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
