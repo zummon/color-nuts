f801c896a5eb51381c9b26ad77e4c0fdabeb0815
zummon <zummon.space@gmail.com>
2026-03-28 15:09:09 +0700

    up

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [svelte()]
+	plugins: [
+		tailwindcss(),
+		svelte()
+	]
 })
